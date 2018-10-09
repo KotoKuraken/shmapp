b6a31ef224bb7a868d9431a47316e8e499c2ddc0
DoubleA176 <austin.alec119@gmail.com>
2018-10-08 21:19:12 -0500

    Added more pages and navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,68 @@ import 'package:flutter/material.dart';
 final GlobalKey<ScaffoldState> _scaffoldKey = new
 GlobalKey<ScaffoldState>();
 
+MyHomePage homePage = new MyHomePage(title: 'Home');
+MyMapPage mapPage = new MyMapPage(title: 'Map');
+MyInfoPage infoPage = new MyInfoPage(title: 'Info');
+MyLoginPage loginPage = new MyLoginPage(title: 'Login');
+
+CreateNav(BuildContext context, bool isHome){
+  return new Column(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      new ListTile(
+        leading: new Icon(Icons.directions_walk),
+        title: new Text('Navigation'),
+        onTap: (){
+          if(!isHome){
+            Navigator.pop(context);
+          }
+        }
+      ),
+      new ListTile(
+          leading: new Icon(Icons.map),
+          title: new Text('Home Map'),
+          onTap: (){
+            if(!isHome){
+              Navigator.pop(context);
+            }
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => mapPage)
+            );
+          }
+      ),
+      new ListTile(
+          leading: new Icon(Icons.info),
+          title: new Text('Info'),
+          onTap: (){
+            if(!isHome){
+              Navigator.pop(context);
+            }
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => infoPage)
+            );
+          }
+      ),
+      new ListTile(
+        leading: new Icon(Icons.person),
+        title: new Text('Faculty Login'),
+        onTap: (){
+          if(!isHome){
+            Navigator.pop(context);
+          }
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => loginPage)
+          );
+        }
+      ),
+    ],
+  );
+}
+
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -22,7 +84,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: new MyHomePage(title: 'Home'),
+      home: homePage,
     );
   }
 }
@@ -88,31 +150,213 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build method
       bottomSheet: new BottomSheet(onClosing: () => null, builder: (BuildContext context) {
 
-        return new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            new ListTile(
-              leading: new Icon(Icons.directions_walk),
-              title: new Text('Navigation'),
-            ),
-            new ListTile(
-            leading: new Icon(Icons.map),
-              title: new Text('Home Map')
-            ),
-            new ListTile(
-              leading: new Icon(Icons.info),
-              title: new Text('Info')
-            ),
-            new ListTile(
-              leading: new Icon(Icons.person),
-              title: new Text('Faculty Login'),
-            ),
-          ],
-        );
+        return CreateNav(context, true);
       }),
 
     );
   }
 }
 
+class MyMapPage extends StatefulWidget {
+  MyMapPage({Key key, this.title}) : super(key: key);
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyMapPageState createState() => new _MyMapPageState();
+}
+
+class _MyMapPageState extends State<MyMapPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug paint" (press "p" in the console where you ran
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+          // window in IntelliJ) to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'This is the Shmapp Map Page',
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build method
+      bottomSheet: new BottomSheet(onClosing: () => null, builder: (BuildContext context) {
+
+        return CreateNav(context, false);
+      }),
+
+    );
+  }
+}
+
+class MyInfoPage extends StatefulWidget {
+  MyInfoPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyInfoPageState createState() => new _MyInfoPageState();
+}
+
+class _MyInfoPageState extends State<MyInfoPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug paint" (press "p" in the console where you ran
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+          // window in IntelliJ) to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'This is the Shmapp Info Page',
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build method
+      bottomSheet: new BottomSheet(onClosing: () => null, builder: (BuildContext context) {
+
+        return CreateNav(context, false);
+      }),
+
+    );
+  }
+}
+
+class MyLoginPage extends StatefulWidget {
+  MyLoginPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyLoginPageState createState() => new _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<MyLoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug paint" (press "p" in the console where you ran
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+          // window in IntelliJ) to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'This is the Shmapp Login Page',
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build method
+      bottomSheet: new BottomSheet(onClosing: () => null, builder: (BuildContext context) {
+
+        return CreateNav(context, false);
+      }),
+
+    );
+  }
+}
